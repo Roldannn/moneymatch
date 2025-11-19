e79c0ae392ac09001470580d6261a89914cb025c
fduarte <frncdrt@gmail.com>
2025-11-18 22:06:35 -0300

    wip

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,7 +2,6 @@
 
 namespace Database\Seeders;
 
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -12,15 +11,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // Primero ejecutar CurrencySeeder para crear las monedas base
         $this->call([
             CurrencySeeder::class,
+            CurrencyEquivalenceSeeder::class,
         ]);
-
-        // Luego ejecutar CurrencyEquivalenceSeeder para scrapear y cargar equivalencias
-        // Nota: Este seeder puede tardar mucho tiempo ya que hace web scraping
-        // $this->call([
-        //     CurrencyEquivalenceSeeder::class,
-        // ]);
     }
 }
